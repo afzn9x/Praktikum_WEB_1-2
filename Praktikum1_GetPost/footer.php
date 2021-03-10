6f5df43ce0aa903bc1921f608dbdcc3dca4ccdf0
Ahmad Fauzan <57349734+FauzanKatil@users.noreply.github.com>
2021-03-11 00:24:34 +0700

    Praktikum 1

---
+++ Praktikum1_GetPost/footer.php
@@ -0,0 +1,8 @@
+        <footer class="footer1">
+            <p class="p2">Ahmad Fauzan - 0110220166</p>
+        </footer>
+    </body>
+    <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/scripts.js"></script>
+</html>
---
+++ Praktikum1_GetPost/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+    <title>Admin</title>
+</head>
+    <body>
+        <header class="header1">
+            <p class="p3"><b>Data Siswa</b></p>
+        </header>
